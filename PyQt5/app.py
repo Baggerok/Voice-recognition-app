78349a2f5a6b1922f5c83ca3f74f82bd9bd3d0d5
Baggerok <baggerf12@gmail.com>
2025-08-04 17:55:42 +0300

    imple,eted button to start AI model

--- PyQt5/app.py
+++ PyQt5/app.py
@@ -2,11 +2,21 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 import sys
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import recognition
+
 def window ():
     app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(0,0,1280,720)
     win.setWindowTitle("My app")
+
+    start_button = QtWidgets.QPushButton(win)
+    start_button.setText("Start recognition")
+    start_button.clicked.connect(recognition.main)
+
 
     win.show()
     sys.exit(app.exec_())
